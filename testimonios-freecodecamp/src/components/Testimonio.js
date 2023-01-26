5b9789c71e42f5f92bbf67d585c665a730ab9647
JuaniOlguin <juani.olguin@gmail.com>
2023-01-26 15:34:33 -0300

    cambio de proyecto y de curso

---
+++ testimonios-freecodecamp/src/components/Testimonio.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Testimonio() {
+  return (
+    <div className='testimonio-container'>
+      <img src="" alt="" className='image-testimonio'/>
+    </div>
+  );
+}
+
+export default Testimonio;
